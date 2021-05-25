1b4a3d979e7b3c045c6809a06624dd5add6b07fa
Luca Severo <luca98.severo@gmail.com>
2021-05-25 15:03:15 -0300

    Create done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,15 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
